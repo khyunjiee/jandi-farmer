47b4f2ebff1536f1064536c96c9f749e7fa24718
Kim HyunJi <khyunjiee@gmail.com>
2021-05-24 22:33:30 +0900

    create: initial project

---
+++ jandifarmer/src/main/java/com/khyunjiee/jandifarmer/JandifarmerApplication.java
@@ -0,0 +1,13 @@
+package com.khyunjiee.jandifarmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JandifarmerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JandifarmerApplication.class, args);
+	}
+
+}
---
+++ jandifarmer/src/test/java/com/khyunjiee/jandifarmer/JandifarmerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khyunjiee.jandifarmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JandifarmerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
