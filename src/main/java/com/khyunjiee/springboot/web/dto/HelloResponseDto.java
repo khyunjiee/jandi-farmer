637af36490021a7503a989fcde304e21eb6c9c87
Kim HyunJi <khyunjiee@gmail.com>
2021-06-21 17:31:38 +0900

    create: 테스트 코드 작성

---
+++ src/main/java/com/khyunjiee/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.khyunjiee.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
