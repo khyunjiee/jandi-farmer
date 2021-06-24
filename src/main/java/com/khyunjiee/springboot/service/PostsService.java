322fa7cb9ba8127c536be8e2751b47175d5fd954
Kim HyunJi <khyunjiee@gmail.com>
2021-06-24 23:50:39 +0900

    create: jpa를 활용해 게시글 작성, 수정 api 구현 & 테스트 코드 작성

---
+++ src/main/java/com/khyunjiee/springboot/service/PostsService.java
@@ -0,0 +1,39 @@
+package com.khyunjiee.springboot.service;
+
+import com.khyunjiee.springboot.domain.posts.Posts;
+import com.khyunjiee.springboot.domain.posts.PostsRepository;
+import com.khyunjiee.springboot.web.dto.PostsResponseDto;
+import com.khyunjiee.springboot.web.dto.PostsSaveRequestDto;
+import com.khyunjiee.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
