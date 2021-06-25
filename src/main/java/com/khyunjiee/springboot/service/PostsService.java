769595da4c91a66e7091f719a1bed6decf74a4f6
Kim HyunJi <khyunjiee@gmail.com>
2021-06-25 16:51:52 +0900

    create: 머스테치로 게시글 조회 페이지 구현

--- src/main/java/com/khyunjiee/springboot/service/PostsService.java
+++ src/main/java/com/khyunjiee/springboot/service/PostsService.java
@@ -2,13 +2,16 @@ package com.khyunjiee.springboot.service;
 
 import com.khyunjiee.springboot.domain.posts.Posts;
 import com.khyunjiee.springboot.domain.posts.PostsRepository;
+import com.khyunjiee.springboot.web.dto.PostsListResponseDto;
 import com.khyunjiee.springboot.web.dto.PostsResponseDto;
 import com.khyunjiee.springboot.web.dto.PostsSaveRequestDto;
 import com.khyunjiee.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +38,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                                        .map(PostsListResponseDto::new)
+                                        .collect(Collectors.toList());
     }
 }
--- src/main/java/com/khyunjiee/springboot/web/IndexController.java
+++ src/main/java/com/khyunjiee/springboot/web/IndexController.java
@@ -1,13 +1,19 @@
 package com.khyunjiee.springboot.web;
 
+import com.khyunjiee.springboot.service.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private final PostsService postsService;
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
